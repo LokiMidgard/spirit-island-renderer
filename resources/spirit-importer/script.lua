11f9d2bf49f15027c53c168299e967d461db03c6
Patrick Kranz <patrick-kranz@live.de>
2020-12-12 10:12:31 +0100

    Fix errors in lua script

--- resources/spirit-importer/script.lua
+++ resources/spirit-importer/script.lua
@@ -1,36 +1,68 @@
 -- lua script that can be added to an object in Tabletop Simulator to load spirits generated with this tool.
 function onLoad() LoadData() end
 
-data = nil
-function LoadData()
+local data = nil
+local json_url = nil
+function LoadData() startLuaCoroutine(self, "LoadDataCo") end
 
-    local url = -- "https://raw.githubusercontent.com/LokiMidgard/spirit-island-custom-spirits/deploy/tabletop.json"
-    self.UI.getXmlTable()[1].children[1].value
-    local function webRequestCallback(webReturn)
-        data = JSON.decode(webReturn.text)
-        startLuaCoroutine(self, "HandleSpiritsCo")
+function LoadDataCo()
+    if (json_url == nil) then
+        while self.UI.getXmlTable() == nil do Wt(0.5) end
+        json_url = self.UI.getXmlTable()[1].children[1].value
     end
-    if (url) then WebRequest.get(url, function(a) webRequestCallback(a) end) end
+    
+    local url = json_url
+    if url == nil then return end
+    local function starts_with(str, start) return str:sub(1, #start) == start end
+
+    if starts_with(url, 'https://') or starts_with(url, 'http://') then
+        local function webRequestCallback(webReturn)
+            data = JSON.decode(webReturn.text)
+            startLuaCoroutine(self, "HandleSpiritsCo")
+        end
+
+        WebRequest.get(url, function(a) webRequestCallback(a) end)
+    else
+        broadcastToAll('Only URLs starting with "http://" or "https://" ares supported',
+                       {r = 1, g = 0, b = 0})
+
+        -- havn't found out a way to open a local file. Propably securety resons...
+        -- local function read_file(path)
+        --     local file = io.open(path, "rb") -- r read mode and b binary mode
+        --     if not file then return nil end
+        --     local content = file:read "*a" -- *a or *all reads the whole file
+        --     file:close()
+        --     return content
+        -- end
+
+        -- local content = read_file(url)
+        -- data = content
+        -- startLuaCoroutine(self, "HandleSpiritsCo")
+    end
+    return 1
 end
+
+function UpdateUrl(player, value, id) json_url = value end
 
 function HandleSpiritsCo()
     local loeadedData = data
     if data == nil then return end
 
-    ui = self.UI.getXmlTable()
-    list = {}
+    local ui = self.UI.getXmlTable()
+    local list = {}
     ui[1].children[3].children[1].children = list
+    ui[1].children[1].value = json_url
 
-    numberOfEntrys = 0
+    local numberOfEntrys = 0
 
     local customAssets = UI.getCustomAssets()
     if customAssets == nil then customAssets = {} end
 
     for key, value in pairs(loeadedData) do
 
-        backFace = value[2].CustomDeck[next(value[2].CustomDeck)].BackURL
+        local backFace = value[2].CustomDeck[next(value[2].CustomDeck)].BackURL
         -- backFace = Next(pairs(value[2].CustomDeck))--.value.BackURL
-        found = false
+        local found = false
         for key2, value2 in pairs(customAssets) do
             if value2.name == key then found = true end
         end
@@ -38,7 +70,7 @@ function HandleSpiritsCo()
             customAssets[#customAssets + 1] = {name = key, url = backFace}
         end
         numberOfEntrys = numberOfEntrys + 1
-        currentElement = {
+        local currentElement = {
             tag = "Panel",
             attributes = {
                 childForceExpandHeight = true,
@@ -76,23 +108,20 @@ function HandleSpiritsCo()
 
     self.setTable("spiritData", loeadedData)
     UI.setCustomAssets(customAssets)
-    wt(0.5)
+    Wt(0.5)
     ui[1].children[3].children[1].attributes.height = numberOfEntrys * 200
 
     self.UI.setXmlTable(ui)
     print("All Spirits loaded")
+    return 1
 end
 
 function LoadSpirit(object_pick, player_color, arg1)
 
-    print(object_pick)
-    print(player_color)
-    print(arg1)
-
-    pos = self.getPosition()
+    local pos = self.getPosition()
     -- the offset is only for the spirit, the power cards look good at there position
-    offset = {x = 4, y = -1.3}
-    value = self.getTable("spiritData")[arg1]
+    local offset = {x = 4, y = -1.3}
+    local value = self.getTable("spiritData")[arg1]
 
     t = {
         position = {x = pos.x + offset.x, y = pos.y + 3, z = pos.z + offset.y},
@@ -105,11 +134,9 @@ function LoadSpirit(object_pick, player_color, arg1)
         json = JSON.encode(value[2])
     }
     spawnObjectJSON(t)
-    print("loaded")
-
 end
 
-function wt(some)
+function Wt(some)
     local Time = os.clock() + some
     while os.clock() < Time do coroutine.yield(0) end
 end
